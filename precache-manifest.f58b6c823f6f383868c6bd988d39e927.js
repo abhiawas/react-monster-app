525a1071fb70cd42b06ecc2c2251e12c84f1560f
abhiawas <abhishekawasthi1310@gmail.com>
2020-03-26 05:36:28 -0400

    Updates

---
+++ precache-manifest.f58b6c823f6f383868c6bd988d39e927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7688350f58a9a4e26e18b42ae6d0c0",
+    "url": "/react-monster-app/index.html"
+  },
+  {
+    "revision": "0fb3376fc4d14ff20166",
+    "url": "/react-monster-app/static/css/main.f4fb6d60.chunk.css"
+  },
+  {
+    "revision": "ea290654e4721d044ac8",
+    "url": "/react-monster-app/static/js/2.bf353e4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-app/static/js/2.bf353e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb3376fc4d14ff20166",
+    "url": "/react-monster-app/static/js/main.e6ade75c.chunk.js"
+  },
+  {
+    "revision": "d58a00dd85d12739fbb9",
+    "url": "/react-monster-app/static/js/runtime-main.fdbd5ea6.js"
+  }
+]);
